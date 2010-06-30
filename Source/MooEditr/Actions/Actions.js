4b11e4d185ec9f2123874e6da8072a85dff4a801
Ryan Mitchell <ryan@rtnetworks.net>
2010-06-30 13:01:24 +0100

    Remove links from Actions.js

--- Source/MooEditr/Actions/Actions.js
+++ Source/MooEditr/Actions/Actions.js
@@ -11,58 +11,12 @@ license: MIT-style license
 
 // extend the language pack
 MooEditr.lang.set({
-	removeHyperlink: 'Remove Hyperlink',
-	addHyperlink: 'Add Hyperlink',
-	selectTextHyperlink: 'Please select the text you wish to hyperlink.',
-	enterURL: 'Enter URL',
 	toggleView: 'Toggle View',
 	insertHorizontalRule: 'Insert Horizontal Rule'
 });
 
 MooEditr.Actions.extend({
 	
-	unlink: {
-		title: MooEditr.lang.get('removeHyperlink')
-	},
-
-	createlink: {
-		title: MooEditr.lang.get('addHyperlink'),
-		options: {
-			shortcut: 'l'
-		},
-		states: {
-			tags: ['a']
-		},
-		dialogs: {
-			alert: MooEditr.UI.AlertDialog.pass(MooEditr.lang.get('selectTextHyperlink')),
-			prompt: function(editor){
-				return MooEditr.UI.PromptDialog(MooEditr.lang.get('enterURL'), 'http://', function(url){
-					editor.execute('createlink', false, url.trim());
-				});
-			}
-		},
-		command: function(){
-			var selection = this.selection;
-			var dialogs = this.dialogs.createlink;
-			if (selection.isCollapsed()){
-				var node = selection.getNode();
-				if (node.get('tag') == 'a' && node.get('href')){
-					selection.selectNode(node);
-					var prompt = dialogs.prompt;
-					prompt.el.getElement('.dialog-input').set('value', node.get('href'));
-					prompt.open();
-				} else {
-					dialogs.alert.open();
-				}
-			} else {
-				var text = selection.getText();
-				var prompt = dialogs.prompt;
-				if (this.urlRegex.test(text)) prompt.el.getElement('.dialog-input').set('value', text);
-				prompt.open();
-			}
-		}
-	},
-
 	toggleview: {
 		title: MooEditr.lang.get('toggleView'),
 		command: function(){
